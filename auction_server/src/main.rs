61041c0f1de776ccbe375dba119af9ffdb673c1d
Matheus Baptistella <mbapts@gmail.com>
2025-01-28 17:36:52 -0300

    feat: add initial test route

--- auction_server/src/main.rs
+++ auction_server/src/main.rs
@@ -1,4 +1,12 @@
+use axum::{routing::get, Router};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let app = router();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+fn router() -> Router {
+    Router::new().route("/", get(|| async { "Hello World!" }))
 }
